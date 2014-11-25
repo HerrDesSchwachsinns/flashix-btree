6d27a8d1f852b76d2473947cfe1b5331ebea52e8
vogl <vogel91@gmx.de>
2014-11-25 10:56:54 +0100

    add another toString

--- src/test/Helper.scala
+++ src/test/Helper.scala
@@ -4,6 +4,7 @@ import datatypes.key.inodekey
 import helpers.scala.Ref
 import layers.IBtree
 import misc.address
+import misc.uninit_address
 
 object Helper {
 	def insert(btree: IBtree,x:Int) {
@@ -11,7 +12,7 @@ object Helper {
 	}
 	def lookup(btree: IBtree,x:Int):Boolean = {
 	  val found = new Ref[Boolean](false)
-	  val adr = new Ref[address](0)
+	  val adr = new Ref[address](uninit_address())
 	  btree.lookup(inodekey(x),adr,found)
 	  if(found.get && adr.get != x) throw new Exception("address != inodekey")
 	  return found.get
